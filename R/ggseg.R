3fbc03a9891196f34767aff281ac5021897e8e0f
rzepkka <rzepkajulia@gmail.com>
2022-05-18 17:03:37 +0200

    clean-up

---
+++ R/ggseg.R
@@ -0,0 +1,166 @@
+library(ggseg3d)
+library(ggseg)
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+library(htmltools)
+library(htmlwidgets)
+
+# Enable this universe
+options(repos = c(
+  ggseg = 'https://ggseg.r-universe.dev',
+  CRAN = 'https://cloud.r-project.org'))
+
+# atlas data
+dk_3d
+
+filtered_dk =dk_3d %>% 
+  filter(surf == "inflated" & hemi == "right") %>% 
+  unnest(cols = ggseg_3d)
+
+filtered_dk$region
+
+aseg_3d
+
+filtered_aseg = aseg_3d %>% 
+  unnest(cols = ggseg_3d)
+
+filtered_aseg
+
+# ============= DK-atlas ========================================================================================
+
+dk_data <- read.csv(file = 'data/dk_R_Subtype 3.csv')
+dk_data
+
+# DK for subtype 0
+
+# make a change for better sequentiol HEX values
+colors = c("indianred4",'indianred2','coral1','lightpink1','mistyrose1')
+
+# LEFT HEMISPHERE
+p = ggseg3d(.data = dk_data, 
+        atlas = dk_3d,
+        hemisphere = 'left',
+        colour = "p", 
+        palette = colors,
+        text = "p") %>% 
+  pan_camera("right medial")
+
+p
+
+# save_html(p, 'plot_htmltools.html', background = "white", libdir = "lib", lang = "en")
+# saveWidget(p, "plot_htmlwidgets.html", selfcontained = F, libdir = "lib")
+
+# RIGHT HEMISPHERE
+ggseg3d(.data = dk_data, 
+        atlas = dk_3d,
+        hemisphere = 'right',
+        colour = "p", 
+        palette = colors,
+        text = "p") %>% 
+  pan_camera("left medial")
+
+# WHOLE BRAIN
+ggseg3d(.data = dk_data, 
+        atlas = dk_3d,
+        hemisphere = c('left','right'),
+        colour = "p", 
+        palette = colors,
+        text = "p",
+        options.legend = list(title=list(text=""))) %>% 
+  pan_camera("left medial")
+
+
+# ============= ASEG ========================================================================================
+
+aseg_data <- read.csv(file = 'data/aseg_R_Subtype 0.csv')
+aseg_data
+
+# without point of reference
+ggseg3d(.data = aseg_data, 
+        atlas = aseg_3d, 
+        colour = "p", 
+        palette = colors,
+        text = "p", 
+        na.alpha= .5,
+        options.legend = list(title=list(text=""))
+  )
+
+# added glassbrain
+ggseg3d(.data = aseg_data, 
+        atlas = aseg_3d, 
+        colour = "p", 
+        palette = colors,
+        text = "p", 
+        na.alpha= .5,
+        options.legend = list(title=list(text=""))) %>% 
+  add_glassbrain()
+
+
+#=========== INPUTS TO 3D SUBPLOTS ========================================================================================
+dk_data <- read.csv(file = 'data/dk_R_Subtype 1.csv')
+aseg_data <- read.csv(file = 'data/aseg_R_Subtype 1.csv')
+
+dk_data
+aseg_data
+
+dk = ggseg3d(.data = dk_data,
+             atlas = dk_3d,
+             hemisphere = c('left','right'),
+             colour = "p",
+             palette = colors,
+             text = "p",
+             options.legend = list(title=list(text="Cortical")),
+             scene = 'scene')
+dk
+
+aseg = ggseg3d(.data = aseg_data, 
+               atlas = aseg_3d, 
+               colour = "p", 
+               palette = colors,
+               text = "p", 
+               # na.alpha= .5,
+               options.legend = list(title=list(text="Subcortical"),dtick=0.1,
+                                     tickformatstops=list(dtickrange=c(0,1))),
+               scene = 'scene2'
+)
+
+
+aseg
+# ========= 3D SUBPLOTS =============================================================================================================================================
+
+fig2 <- subplot(dk, aseg)
+fig2 <- fig2 %>% layout(title = "Subtype 0 - 3D Visualization of the disease timeline",
+                        scene = list(domain=list(x=c(0,1),y=c(0.5,1)),
+                                     aspectmode='auto',
+                                     xaxis=list(backgroundcolor='white')
+                                     ),
+                        scene2 = list(domain=list(x=c(0,1),y=c(0,0.5)),
+                                      aspectmode='auto'
+                        )) 
+fig2 
+
+# SAVE 3D PLOTS TO LOAD THEM INTO STREAMLIT 
+
+saveWidget(fig2, "html_3D/Subtype 0.html", selfcontained = F, libdir = "lib0")
+
+
+# =============================
+
+p <- plot_ly(mtcars, x = ~wt, y = ~mpg, color = ~cyl)
+
+# pass any colorbar attribute -- 
+# https://plotly.com/r/reference/#scatter-marker-colorbar
+colorbar(p, len = 0.5)
+
+# Expand the limits of the colorbar
+colorbar(p, limits = c(0, 20))
+# values outside the colorbar limits are considered "missing"
+colorbar(p, limits = c(5, 6))
+
+# also works on colorbars generated via a z value
+corr <- cor(diamonds[vapply(diamonds, is.numeric, logical(1))])
+plot_ly(x = rownames(corr), y = colnames(corr), z = corr) %>%
+  add_heatmap() %>%
+  colorbar(limits = c(-1, 1))
+
--- R/ggseg_snowphlake.R
+++ R/ggseg_snowphlake.R
@@ -5,128 +5,133 @@ library(dplyr)
 library(tidyr)
 library(htmltools)
 library(htmlwidgets)
-
-# Enable this universe
-options(repos = c(
-  ggseg = 'https://ggseg.r-universe.dev',
-  CRAN = 'https://cloud.r-project.org'))
-
-# atlas data
-dk_3d
-
-filtered_dk =dk_3d %>% 
-  filter(surf == "inflated" & hemi == "right") %>% 
-  unnest(cols = ggseg_3d)
-
-filtered_dk$region
-
-aseg_3d
-
-filtered_aseg = aseg_3d %>% 
-  unnest(cols = ggseg_3d)
-
-filtered_aseg
-
-# ============= DK-atlas ========================================================================================
-
-dk_data <- read.csv(file = 'data/dk_R_Subtype 3.csv')
-dk_data
-
-# DK for subtype 0
-
-# make a change for better sequentiol HEX values
-colors = c("indianred4",'indianred2','coral1','lightpink1','mistyrose1')
-
-# LEFT HEMISPHERE
-p = ggseg3d(.data = dk_data, 
-        atlas = dk_3d,
-        hemisphere = 'left',
-        colour = "p", 
-        palette = colors,
-        text = "p") %>% 
-  pan_camera("right medial")
-
-p
-
-# save_html(p, 'plot_htmltools.html', background = "white", libdir = "lib", lang = "en")
-# saveWidget(p, "plot_htmlwidgets.html", selfcontained = F, libdir = "lib")
-
-# RIGHT HEMISPHERE
-ggseg3d(.data = dk_data, 
-        atlas = dk_3d,
-        hemisphere = 'right',
-        colour = "p", 
-        palette = colors,
-        text = "p") %>% 
-  pan_camera("left medial")
-
-# WHOLE BRAIN
-ggseg3d(.data = dk_data, 
-        atlas = dk_3d,
-        hemisphere = c('left','right'),
-        colour = "p", 
-        palette = colors,
-        text = "p",
-        options.legend = list(title=list(text=""))) %>% 
-  pan_camera("left medial")
+library(plotly)
 
 
-# ============= ASEG ========================================================================================
+# ========== CUSTOM GGSEG_3D =======================================
+custom_ggseg <- function(.data=NULL, atlas="dk_3d",
+                    surface = "LCBC", hemisphere = c("right","subcort"),
+                    label = "region", text = NULL, colour = "colour",
+                    palette = NULL, na.colour = "darkgrey", na.alpha = 1,
+                    show.legend = TRUE, options.legend = NULL, ...) {
+  
+  
+  # Grab the atlas, even if it has been provided as character string
+  atlas3d = get_atlas(atlas,
+                      surface = surface,
+                      hemisphere = hemisphere)
+  
+  # If data has been supplied, merge it
+  if(!is.null(.data)){
+    atlas3d <- data_merge(.data, atlas3d)
+  }
+  
+  pal.colours <- get_palette(palette)
+  
+  # If colour column is numeric, calculate the gradient
+  if(is.numeric(unlist(atlas3d[,colour]))){
+    
+    if(is.null(names(palette))){
+      pal.colours$values <- seq(0,1,length.out = nrow(pal.colours))
+    }
+    
+    atlas3d$new_col = gradient_n_pal(pal.colours$orig, pal.colours$values,"Lab")(
+      unlist(atlas3d[,colour]))
+    fill = "new_col"
+    
+  }else{
+    fill = colour
+  }
+  
+  # initiate plot
+  p = plotly::plot_ly()
+  
+  # add one trace per file inputed
+  for(tt in 1:nrow(atlas3d)){
+    
+    col = rep(unlist(atlas3d[tt, fill]), nrow(atlas3d$mesh[[tt]]$faces))
+    
+    col = ifelse(is.na(col), na.colour, col)
+    
+    op = ifelse(is.na(unlist(atlas3d[tt, fill])), na.alpha, 1)
+    
+    txt = if(is.null(text)){
+      text
+    }else{
+      paste0(text, ": ", unlist(atlas3d[tt, text]))
+    }
+    
+    p = plotly::add_trace(p,
+                          x = atlas3d$mesh[[tt]]$vertices$x,
+                          y = atlas3d$mesh[[tt]]$vertices$y,
+                          z = atlas3d$mesh[[tt]]$vertices$z,
+                          
+                          i = atlas3d$mesh[[tt]]$faces$i-1,
+                          j = atlas3d$mesh[[tt]]$faces$j-1,
+                          k = atlas3d$mesh[[tt]]$faces$k-1,
+                          
+                          facecolor = col,
+                          type = "mesh3d",
+                          text = txt,
+                          showscale = FALSE,
+                          opacity = op,
+                          name = unlist(atlas3d[tt, label]),
+                          ...
+    )
+  }
+  
+  # work around to get legend
+  if(show.legend & is.numeric(unlist(atlas3d[,colour]))){
+    
+    dt_leg <- dplyr::mutate(pal.colours,
+                            x = 0, y = 0, z = 0)
+    p <- plotly::add_trace(p, data = dt_leg,
+                           x = ~ x, y = ~ y, z = ~ z,
+                           
+                           intensity =  ~ values,
+                           colorscale =  unname(dt_leg[,c("norm", "hex")]),
+                           type = "mesh3d",
+                           colorbar = options.legend
+    )
+  }
+  
+  p
+}
 
-aseg_data <- read.csv(file = 'data/aseg_R_Subtype 0.csv')
-aseg_data
+# =========== ADD CUSTOM FUNCTIONS TO ENVIRONMENT ==============================
 
-# without point of reference
-ggseg3d(.data = aseg_data, 
-        atlas = aseg_3d, 
-        colour = "p", 
-        palette = colors,
-        text = "p", 
-        na.alpha= .5,
-        options.legend = list(title=list(text=""))
-  )
-
-# added glassbrain
-ggseg3d(.data = aseg_data, 
-        atlas = aseg_3d, 
-        colour = "p", 
-        palette = colors,
-        text = "p", 
-        na.alpha= .5,
-        options.legend = list(title=list(text=""))) %>% 
-  add_glassbrain()
-
+environment(custom_ggseg) <- asNamespace('ggseg3d')
 
 #=========== INPUTS TO 3D SUBPLOTS ========================================================================================
 dk_data <- read.csv(file = 'data/dk_R_slider_Subtype 4.csv')
 aseg_data <- read.csv(file = 'data/aseg_R_slider_Subtype 4.csv')
 
-dk_data
-aseg_data
+# make a change for better sequentiol HEX values
+colors = c("indianred4",'indianred2','coral1','lightpink1','mistyrose1')
 
-dk = ggseg3d(.data = dk_data,
+dk = custom_ggseg(.data = dk_data,
              atlas = dk_3d,
              hemisphere = c('left','right'),
              colour = "p",
              palette = colors,
              text = "p",
-             options.legend = list(title=list(text="Cortical")),
+             options.legend = list(title=list(text="Cortical"),dtick=0.1,
+                              tickformatstops=list(dtickrange=c(0,1))),
              scene = 'scene')
 dk
 
-aseg = ggseg3d(.data = aseg_data, 
+aseg = custom_ggseg(.data = aseg_data, 
                atlas = aseg_3d, 
                colour = "p", 
                palette = colors,
                text = "p", 
-               # na.alpha= .5,
                options.legend = list(title=list(text="Subcortical"),dtick=0.1,
                                      tickformatstops=list(dtickrange=c(0,1))),
                scene = 'scene2'
 )
 
-
 aseg
+
 # ========= 3D SUBPLOTS =============================================================================================================================================
 
 fig2 <- subplot(dk, aseg)
@@ -134,7 +139,7 @@ fig2 <- fig2 %>% layout(title = "Subtype 4 - 3D Visualization of the disease tim
                         scene = list(domain=list(x=c(0,1),y=c(0.5,1)),
                                      aspectmode='auto',
                                      xaxis=list(backgroundcolor='white')
-                                     ),
+                        ),
                         scene2 = list(domain=list(x=c(0,1),y=c(0,0.5)),
                                       aspectmode='auto'
                         )) 
@@ -142,25 +147,5 @@ fig2
 
 # SAVE 3D PLOTS TO LOAD THEM INTO STREAMLIT 
 
-saveWidget(fig2, "html_3D/slider/Subtype 4.html", selfcontained = F, libdir = "lib")
-
-
-# =============================
-
-p <- plot_ly(mtcars, x = ~wt, y = ~mpg, color = ~cyl)
-
-# pass any colorbar attribute -- 
-# https://plotly.com/r/reference/#scatter-marker-colorbar
-colorbar(p, len = 0.5)
-
-# Expand the limits of the colorbar
-colorbar(p, limits = c(0, 20))
-# values outside the colorbar limits are considered "missing"
-colorbar(p, limits = c(5, 6))
-
-# also works on colorbars generated via a z value
-corr <- cor(diamonds[vapply(diamonds, is.numeric, logical(1))])
-plot_ly(x = rownames(corr), y = colnames(corr), z = corr) %>%
-  add_heatmap() %>%
-  colorbar(limits = c(-1, 1))
+saveWidget(fig2, "html_3D/slider/Subtype_4.html", selfcontained = F, libdir = "sub4")
 
