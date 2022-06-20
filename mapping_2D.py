98c617e0fd5bd666919748332d8f670eab7d9686
rzepkka <rzepkajulia@gmail.com>
2022-06-20 11:04:13 +0200

    thesis clean-up

--- mapping_2D.py
+++ mapping_2D.py
@@ -7,6 +7,53 @@ from PIL import Image
 
 # ====================================== MAPPINGS ==================================================================================================================
 
+# def dk_dict(T,S, mapped_dict, subtype_labels = None, subtype = None):
+    
+#     """
+#     Creates a dictionary, which can be used as input to ggseg.plot_dk() function
+#     :param T: dataframe from dk_dataframe() function
+#     :param S: chosen subtype
+#     :param mapped_dict: a dictionary with key: values --> T.biomarker_labels: list(DK-labels)
+#     :param subtype: name or index of the subtype from subtype_lables (optional, choses first available subtype as default)  
+#     :param subtype_labels: a list with names of the subtypes (optional)
+#     :return: dictionary with scores for each DK region for chosen subtype
+#     """
+    
+#     unique_subtypes = np.unique(S['subtypes'][~np.isnan(S['subtypes'])])
+#     if subtype_labels is None:
+#         subtype_labels = {f'Subtype {i}': i for i in range(len(unique_subtypes))}
+#         if subtype is None:
+#             subtype = next(iter(subtype_labels))
+#     elif subtype is None:
+#         subtype = subtype_labels[0]  
+           
+#     # clean names from capital letters
+#     labels = list(map(lambda x: x.lower(), T.biomarker_labels))
+#     labels_dict = {num: label.lower() for num, label in enumerate(labels)}
+    
+#     order = T.sequence_model['ordering'][subtype_labels[subtype]]
+    
+#     labels_ordered = []
+#     for o in order:
+#         labels_ordered.append(labels_dict[o])   
+    
+#     # All regions
+#     dic = dict(zip(labels_ordered, T.sequence_model['event_centers'][subtype_labels[subtype]]))
+                    
+#     # flat list of dict values (single list of DK-labels)
+#     dk_flat = [x for v in mapped_dict.values() for x in v]
+                
+#     # Match T.biomarker_labels to DK labels
+#     list_plot = list()
+#     for key in mapped_dict.keys():
+#             for item in mapped_dict[key]:
+#                 list_plot.append(dic[key])
+                    
+#     # Dict for dk-label: T.label value
+#     dic_dk = dict(zip(dk_flat, list_plot))
+    
+#     return dic_dk
+
 def dk_dict(T,S, mapped_dict, subtype_labels = None, subtype = None):
     
     """
@@ -24,26 +71,29 @@ def dk_dict(T,S, mapped_dict, subtype_labels = None, subtype = None):
         subtype_labels = {f'Subtype {i}': i for i in range(len(unique_subtypes))}
         if subtype is None:
             subtype = next(iter(subtype_labels))
-    elif subtype is None:
+    else:
+        subtype_labels = {subtype_labels[i]: i for i in range(len(unique_subtypes))}
+    
+    if subtype is None:
         subtype = subtype_labels[0]  
-           
+               
     # clean names from capital letters
     labels = list(map(lambda x: x.lower(), T.biomarker_labels))
     labels_dict = {num: label.lower() for num, label in enumerate(labels)}
     
-    order = T.sequence_model['ordering'][subtype_labels[subtype]]
-    
+    # order the regions
+    order = T.sequence_model['ordering'][subtype_labels[subtype]]   
     labels_ordered = []
     for o in order:
         labels_ordered.append(labels_dict[o])   
     
-    # All regions
+    # get event centers for all regions
     dic = dict(zip(labels_ordered, T.sequence_model['event_centers'][subtype_labels[subtype]]))
                     
     # flat list of dict values (single list of DK-labels)
     dk_flat = [x for v in mapped_dict.values() for x in v]
                 
-    # Match T.biomarker_labels to DK labels
+    #Match T.biomarker_labels to DK labels
     list_plot = list()
     for key in mapped_dict.keys():
             for item in mapped_dict[key]:
@@ -54,6 +104,52 @@ def dk_dict(T,S, mapped_dict, subtype_labels = None, subtype = None):
     
     return dic_dk
 
+
+# def aseg_dict(T, S, mapped_dict, subtype_labels = None, subtype = None, hemi = False):
+    
+#     """
+#     Creates a dictionary, which can be used as input to ggseg.plot_dk() function
+#     :param T: Timeline object
+#     :param S: subtyping dictionary, subtypes for each patient individually
+#     :param mapped_dict: a dictionary with key: values --> T.biomarker_labels: list(DK-labels)
+#     :param subtype_labels: a list with names of the subtypes (optional)
+#     :param subtype: name or index of the subtype from subtype_lables (optional, choses first available subtype as default)  
+#     :return: dictionary with scores for each DK region for chosen subtype
+#     """
+
+#     unique_subtypes = np.unique(S['subtypes'][~np.isnan(S['subtypes'])])
+#     if subtype_labels is None:
+#         subtype_labels = {f'Subtype {i}': i for i in range(len(unique_subtypes))}
+#         if subtype is None:
+#             subtype = next(iter(subtype_labels))
+#     elif subtype is None:
+#         subtype = subtype_labels[0]
+    
+#     # clean names from capital letters
+#     labels = T.biomarker_labels
+#     labels_dict = {num: label.lower() for num, label in enumerate(labels)}
+#     order = T.sequence_model['ordering'][subtype_labels[subtype]]
+    
+#     labels_ordered = []
+#     for o in order:
+#         labels_ordered.append(labels_dict[o])    
+     
+#     # Dictionary with all labels
+#     dic = dict(zip(labels_ordered, T.sequence_model['event_centers'][subtype_labels[subtype]]))
+    
+#     # flat list of dict values (single list of DK-labels)
+#     aseg_flat = [x for v in mapped_dict.values() for x in v]
+            
+#     #Match T.biomarker_labels to DK labels
+#     list_plot = list()
+#     for key in mapped_dict.keys():
+#         for item in mapped_dict[key]:
+#             list_plot.append(dic[key])
+            
+#     # Dict for dk-label: T.label value
+#     dic_aseg = dict(zip(aseg_flat, list_plot))    
+        
+#     return dic_aseg
 
 def aseg_dict(T, S, mapped_dict, subtype_labels = None, subtype = None, hemi = False):
     
@@ -72,14 +168,18 @@ def aseg_dict(T, S, mapped_dict, subtype_labels = None, subtype = None, hemi = F
         subtype_labels = {f'Subtype {i}': i for i in range(len(unique_subtypes))}
         if subtype is None:
             subtype = next(iter(subtype_labels))
-    elif subtype is None:
-        subtype = subtype_labels[0]
+    else:
+        subtype_labels = {subtype_labels[i]: i for i in range(len(unique_subtypes))}
+    
+    if subtype is None:
+        subtype = subtype_labels[0]  
     
     # clean names from capital letters
     labels = T.biomarker_labels
     labels_dict = {num: label.lower() for num, label in enumerate(labels)}
     order = T.sequence_model['ordering'][subtype_labels[subtype]]
     
+    # order the regions
     labels_ordered = []
     for o in order:
         labels_ordered.append(labels_dict[o])    
--- mapping_3D.py
+++ mapping_3D.py
@@ -6,7 +6,7 @@ from PIL import Image
 
 
 
-def save_subtype_data(T, S, map_dk, map_aseg):  
+def save_subtype_data(T, S, map_dk, map_aseg, subtype_labels=None):  
     """
     Saves all subtypes data in separate .csv files for DK and ASEG atlas
     :param T: Timeline object
@@ -18,20 +18,23 @@ def save_subtype_data(T, S, map_dk, map_aseg):
     
     # Get subtype labels
     unique_subtypes = np.unique(S['subtypes'][~np.isnan(S['subtypes'])])
-    subtype_labels = []
-    for i in range(len(unique_subtypes)):
-        subtype_labels.append('Subtype '+str(int(unique_subtypes[i])))  
+    if subtype_labels is None:
+        subtype_labels = []
+        for i in range(len(unique_subtypes)):
+            subtype_labels.append('Subtype '+str(int(unique_subtypes[i])))  
         
     # Save each file as csv
     for label in subtype_labels:
         
         dk = dk_3D(T, S, 
                    mapped_dict = map_dk, 
-                   subtype = label)
+                   subtype = label,
+                   subtype_labels=subtype_labels)
         
         aseg = aseg_3D(T, S, 
                        mapped_dict = map_aseg, 
-                       subtype = label) 
+                       subtype = label,
+                       subtype_labels=subtype_labels) 
 
         dk.to_csv(f'temp_folder/csv/dk_R_{label}.csv', index = False)
         aseg.to_csv(f'temp_folder/csv/aseg_R_{label}.csv', index = False)    
@@ -48,6 +51,63 @@ def safe_html(labels, command, path2script):
 
 
 # ====================================== DK-ATLAS ==================================================================================================================
+
+# def dk_3D(T,S, mapped_dict, subtype_labels = None, subtype = None):
+    
+#     """
+#     Creates a dictionary, which can be used as input to ggseg3d() function
+#     :param T: Timeline object
+#     :param S: subtyping dictionary, subtypes for each patient individually
+#     :param mapped_dict: dictionary from loaded JSON file, key: values --> T.biomarker_labels: list(DK-labels)
+#     :param subtype_labels: a list with names of the subtypes (optional)
+#     :param subtype: name or index of the subtype from subtype_lables (optional, choses first available subtype as default)  
+#     :return: dictionary with scores for each DK region for chosen subtype
+#     """
+    
+#     unique_subtypes = np.unique(S['subtypes'][~np.isnan(S['subtypes'])])
+#     if subtype_labels is None:
+#         subtype_labels = {f'Subtype {i}': i for i in range(len(unique_subtypes))}
+#         if subtype is None:
+#             subtype = next(iter(subtype_labels))
+#     elif subtype is None:
+#         subtype = subtype_labels[0]  
+        
+#     # clean names from capital letters
+#     labels = list(map(lambda x: x.lower(), T.biomarker_labels))
+#     labels_dict = {num: label.lower() for num, label in enumerate(labels)}
+    
+#     order = T.sequence_model['ordering'][subtype_labels[subtype]]
+    
+#     labels_ordered = []
+#     for o in order:
+#         labels_ordered.append(labels_dict[o])       
+    
+#     dic = dict(zip(labels_ordered, T.sequence_model['event_centers'][subtype_labels[subtype]]))
+                    
+#     # flat lost of dict values (single list of DK-labels)
+#     dk_flat = [x for v in mapped_dict.values() for x in v]
+        
+#     hemi = []
+#     for idx, region in enumerate(dk_flat):
+#         if 'left' in region:
+#             hemi.append('left')
+#             dk_flat[idx]=dk_flat[idx].replace(' left','')
+#         elif 'right' in region:
+#             hemi.append('right')
+#             dk_flat[idx]=dk_flat[idx].replace(' right','')
+#         else:
+#             hemi.append('subcort')
+              
+#     #Match T.biomarker_labels to DK labels
+#     list_plot = list()
+#     for key in mapped_dict.keys():
+#         for item in mapped_dict[key]:
+#             list_plot.append(dic[key])
+    
+#     dic_dk = {'region': dk_flat, 'hemi':hemi, 'p': list_plot}
+#     df = pd.DataFrame(dic_dk)
+    
+#     return df
 
 def dk_3D(T,S, mapped_dict, subtype_labels = None, subtype = None):
     
@@ -66,24 +126,29 @@ def dk_3D(T,S, mapped_dict, subtype_labels = None, subtype = None):
         subtype_labels = {f'Subtype {i}': i for i in range(len(unique_subtypes))}
         if subtype is None:
             subtype = next(iter(subtype_labels))
-    elif subtype is None:
+    else:
+        subtype_labels = {subtype_labels[i]: i for i in range(len(unique_subtypes))}
+    
+    if subtype is None:
         subtype = subtype_labels[0]  
         
     # clean names from capital letters
     labels = list(map(lambda x: x.lower(), T.biomarker_labels))
     labels_dict = {num: label.lower() for num, label in enumerate(labels)}
     
-    order = T.sequence_model['ordering'][subtype_labels[subtype]]
-    
+    # order the regions
+    order = T.sequence_model['ordering'][subtype_labels[subtype]] 
     labels_ordered = []
     for o in order:
-        labels_ordered.append(labels_dict[o])       
-    
+        labels_ordered.append(labels_dict[o])   
+         
+           
     dic = dict(zip(labels_ordered, T.sequence_model['event_centers'][subtype_labels[subtype]]))
                     
     # flat lost of dict values (single list of DK-labels)
     dk_flat = [x for v in mapped_dict.values() for x in v]
-        
+    
+    # Separate hemispheres
     hemi = []
     for idx, region in enumerate(dk_flat):
         if 'left' in region:
@@ -94,13 +159,14 @@ def dk_3D(T,S, mapped_dict, subtype_labels = None, subtype = None):
             dk_flat[idx]=dk_flat[idx].replace(' right','')
         else:
             hemi.append('subcort')
-              
+                
     #Match T.biomarker_labels to DK labels
     list_plot = list()
     for key in mapped_dict.keys():
         for item in mapped_dict[key]:
             list_plot.append(dic[key])
-    
+            
+    # create DataFrame
     dic_dk = {'region': dk_flat, 'hemi':hemi, 'p': list_plot}
     df = pd.DataFrame(dic_dk)
     
@@ -108,6 +174,54 @@ def dk_3D(T,S, mapped_dict, subtype_labels = None, subtype = None):
 
 
 # ====================================== ASEG atlas ==================================================================================================================
+
+# def aseg_3D(T, S, mapped_dict, subtype_labels = None, subtype = None):
+    
+#     """
+#     Creates a dictionary, which can be used as input to ggseg.plot_dk() function
+#     :param T: Timeline object
+#     :param S: subtyping dictionary, subtypes for each patient individually
+#     :param mapped_dict: dictionary from loaded JSON file, key: values --> T.biomarker_labels: list(DK-labels)
+#     :param subtype_labels: a list with names of the subtypes (optional)
+#     :param subtype: name or index of the subtype from subtype_lables (optional, choses first available subtype as default)  
+#     :return: dictionary with scores for each DK region for chosen subtype
+#     """
+
+#     unique_subtypes = np.unique(S['subtypes'][~np.isnan(S['subtypes'])])
+#     if subtype_labels is None:
+#         subtype_labels = {f'Subtype {i}': i for i in range(len(unique_subtypes))}
+#         if subtype is None:
+#             subtype = next(iter(subtype_labels))
+#     elif subtype is None:
+#         subtype = subtype_labels[0]
+        
+#     labels = T.biomarker_labels
+#     labels_dict = {num: label.lower() for num, label in enumerate(labels)}
+#     order = T.sequence_model['ordering'][subtype_labels[subtype]]
+        
+#     labels_ordered = []
+#     for o in order:
+#         labels_ordered.append(labels_dict[o])
+        
+#     # Dictionary with all labels
+#     dic = dict(zip(labels_ordered, T.sequence_model['event_centers'][subtype_labels[subtype]]))
+    
+#     # flat list of dict values (single list of DK-labels)
+#     aseg_flat = [x for v in mapped_dict.values() for x in v]
+    
+#     #Match T.biomarker_labels to DK labels
+#     list_plot = list()
+#     for key in mapped_dict.keys():
+#         for item in mapped_dict[key]:
+#             list_plot.append(dic[key])
+            
+#     # Dict for dk-label: T.label value
+#     dic_aseg = {'region': aseg_flat, 'p':list_plot} 
+    
+#     df = pd.DataFrame(dic_aseg)
+        
+#     return df
+
 
 def aseg_3D(T, S, mapped_dict, subtype_labels = None, subtype = None):
     
@@ -126,13 +240,18 @@ def aseg_3D(T, S, mapped_dict, subtype_labels = None, subtype = None):
         subtype_labels = {f'Subtype {i}': i for i in range(len(unique_subtypes))}
         if subtype is None:
             subtype = next(iter(subtype_labels))
-    elif subtype is None:
-        subtype = subtype_labels[0]
+    else:
+        subtype_labels = {subtype_labels[i]: i for i in range(len(unique_subtypes))}
+    
+    if subtype is None:
+        subtype = subtype_labels[0] 
         
+    # clean label names
     labels = T.biomarker_labels
     labels_dict = {num: label.lower() for num, label in enumerate(labels)}
     order = T.sequence_model['ordering'][subtype_labels[subtype]]
         
+    # order the regions
     labels_ordered = []
     for o in order:
         labels_ordered.append(labels_dict[o])
@@ -149,9 +268,8 @@ def aseg_3D(T, S, mapped_dict, subtype_labels = None, subtype = None):
         for item in mapped_dict[key]:
             list_plot.append(dic[key])
             
-    # Dict for dk-label: T.label value
-    dic_aseg = {'region': aseg_flat, 'p':list_plot} 
-    
+    # create DataFrame
+    dic_aseg = {'region': aseg_flat, 'p':list_plot}     
     df = pd.DataFrame(dic_aseg)
         
     return df
